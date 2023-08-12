d5404d22c98be1bbf726e7ec47395809d573df0b
ObaniGemini <robin@pi-et-ro.net>
2023-08-12 15:41:42 +0200

    unfinished menu

---
+++ scenes/main/main_menu_button.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+signal selected
+signal unselected
+signal pressed
+
+var is_selected = false
+
+func select():
+	is_selected = true
+	emit_signal("selected")
+
+func unselect():
+	is_selected = false
+	emit_signal("unselected")
+	for body in get_children():
+		if body is Area2D:
+			body.unselect()
+
+func _ready():
+	mouse_entered.connect(select)
+	mouse_exited.connect(unselect)
+
+
+func _input(event):
+	if !is_selected:
+		return
+	
+	if event is InputEventMouseButton:
+		if event.pressed:
+			emit_signal("pressed")
+	
+	if event.is_action_pressed("ui_accept"):
+		emit_signal("pressed")
--- scenes/main/menu.gd
+++ scenes/main/menu.gd
@@ -1,10 +1,77 @@
 extends Node2D
 
+const TWEEN_TIME_MIN = 0.1
+const TWEEN_TIME_MAX = 0.4
+
 const WORLD_PATH = "res://scenes/main/world.tscn"
+
+var selected = null
+
+func t_time():
+	return randf_range(TWEEN_TIME_MIN, TWEEN_TIME_MAX)
+
+func tween_btn(obj, prop, pos):
+	var t = get_tree().create_tween().bind_node(self)
+	t.tween_property(obj, prop, pos, randf_range(TWEEN_TIME_MIN, TWEEN_TIME_MAX)).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+
+
+
+
+func select_play():
+	tween_btn($Play/Sprite2D, "position:y", -80)
+	tween_btn($Play/Sprite2D2, "position:y", 80)
+
+func unselect_play():
+	tween_btn($Play/Sprite2D, "position:y", -64)
+	tween_btn($Play/Sprite2D2, "position:y", 64)
+
+
+
+
+func select_settings():
+	tween_btn($Settings/Sprite2D, "position:y", -256)
+	tween_btn($Settings/Sprite2D2, "position:y", 256)
+
+func unselect_settings():
+	tween_btn($Settings/Sprite2D, "position:y", -64)
+	tween_btn($Settings/Sprite2D2, "position:y", 64)
+
+
+
+
+func select_quit():
+	tween_btn($Quit/Node2D/Sprite2D, "position:y", 24)
+	tween_btn($Quit/Node2D/Sprite2D2, "position:y", -24)
+	tween_btn($Quit/Node2D/Sprite2D3, "position:x", -24)
+	tween_btn($Quit/Node2D/Sprite2D4, "position:x", 24)
+
+func unselect_quit():
+	tween_btn($Quit/Node2D/Sprite2D, "position:y", 0)
+	tween_btn($Quit/Node2D/Sprite2D2, "position:y", 0)
+	tween_btn($Quit/Node2D/Sprite2D3, "position:x", 0)
+	tween_btn($Quit/Node2D/Sprite2D4, "position:x", 0)
+
+
+
+
+func _ready():
+	$Play.pressed.connect(play)
+	$Play.selected.connect($Settings.unselect)
+	$Play.selected.connect($Quit.unselect)
+	
+#	$Settings.pressed.connect(quit)
+	$Settings.selected.connect($Play.unselect)
+	$Settings.selected.connect($Quit.unselect)
+	
+	$Quit.pressed.connect(quit)
+	$Quit.selected.connect($Play.unselect)
+	$Quit.selected.connect($Settings.unselect)
+
+
+
 
 func play():
 	get_tree().change_scene_to_file(WORLD_PATH)
-
 
 func quit():
 	print("Bye")
