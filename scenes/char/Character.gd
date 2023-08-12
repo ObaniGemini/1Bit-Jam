b3c4a24d4803991413613eb2affd1ce73f3b401a
ObaniGemini <robin@pi-et-ro.net>
2023-08-12 09:50:54 +0200

    Add twinstick controls if we ever need some

--- scenes/char/Character.gd
+++ scenes/char/Character.gd
@@ -9,16 +9,31 @@ const JOYSTICK_SHOOT_MIN_LENGTH = 0.6
 var move_dir : Vector2 = Vector2(0, 0)
 var shoot_dir : Vector2 = Vector2(0, 0)
 
+var shooting = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	debug_log()
+	
 	velocity = move_dir * SPEED
+	rotation = shoot_dir.angle()
+	
 	move_and_slide()
 
 func joy_axis_move(value):
 	if abs(value) < JOYSTICK_MOVE_THRESHOLD: return 0.0
+	return value
+
+func joy_axis_shoot(value, original):
+	shooting = true
+	if abs(value) < JOYSTICK_SHOOT_THRESHOLD:
+		if shoot_dir.length() > JOYSTICK_SHOOT_MIN_LENGTH:
+			return 0.0
+		else:
+			shooting = false
+			return original
 	return value
 
 func dbg_vec(vec):
@@ -32,6 +47,9 @@ func _input(event):
 	if event is InputEventJoypadMotion:
 		if event.axis == JOY_AXIS_LEFT_X: move_dir.x = joy_axis_move(event.axis_value)
 		elif event.axis == JOY_AXIS_LEFT_Y: move_dir.y = joy_axis_move(event.axis_value)
+		
+		if event.axis == JOY_AXIS_RIGHT_X: shoot_dir.x = joy_axis_shoot(event.axis_value, shoot_dir.x)
+		elif event.axis == JOY_AXIS_RIGHT_Y: shoot_dir.y = joy_axis_shoot(event.axis_value, shoot_dir.y)
 	else:
 		if event.is_action_pressed("ui_left"): move_dir.x -= 1
 		elif event.is_action_released("ui_left"): move_dir.x += 1
@@ -42,6 +60,33 @@ func _input(event):
 		elif event.is_action_released("ui_up"): move_dir.y += 1
 		if event.is_action_pressed("ui_down"): move_dir.y += 1
 		elif event.is_action_released("ui_down"): move_dir.y -= 1
+		
+#		if event.is_action_pressed("aim_left"):
+#			shooting = true
+#			shoot_dir.x -= 1
+#		elif event.is_action_released("aim_left"):
+#			shoot_dir.x += 1
+#
+#		if event.is_action_pressed("aim_right"):
+#			shooting = true
+#			shoot_dir.x += 1
+#		elif event.is_action_released("aim_right"):
+#			shoot_dir.x -= 1
+#
+#		if event.is_action_pressed("aim_up"):
+#			shooting = true
+#			shoot_dir.y -= 1
+#		elif event.is_action_released("aim_up"):
+#			shoot_dir.y += 1
+#
+#		if event.is_action_pressed("aim_down"):
+#			shooting = true
+#			shoot_dir.y += 1
+#		elif event.is_action_released("aim_down"):
+#			shoot_dir.y -= 1
+#
+#		if shoot_dir.length() < JOYSTICK_SHOOT_MIN_LENGTH:
+#			shooting = false
 	
 	move_dir.x = clampf(move_dir.x, -1, 1)
 	move_dir.y = clampf(move_dir.y, -1, 1)
