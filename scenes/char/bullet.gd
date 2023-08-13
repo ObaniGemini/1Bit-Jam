f39a8c97ab8e36c10f849e1b53fdc3fe8576b885
ObaniGemini <robin@pi-et-ro.net>
2023-08-13 16:44:16 +0200

    Start with level2

--- scenes/char/bullet.gd
+++ scenes/char/bullet.gd
@@ -23,6 +23,8 @@ func _on_timer_timeout():
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("enemy"):
-		$Area2D.disconnect("body_entered", _on_area_2d_body_entered)
-		destroy()
+	if body.is_in_group("player"):
+		return
+	
+	$Area2D.disconnect("body_entered", _on_area_2d_body_entered)
+	destroy()
--- scenes/levels/Level2.gd
+++ scenes/levels/Level2.gd
@@ -4,9 +4,4 @@ extends "res://scenes/main/level.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Ship.set_camera_mode($Ship.Camera_Follow)
-	
-	for c in $WorldBoundary.get_children():
-		var n = c.get_child(0)
-		n.scale = c.shape.size
-		n.modulate = Color(0, 0, 0)
-
+	$TileMap.modulate = Color(0, 0, 0)
