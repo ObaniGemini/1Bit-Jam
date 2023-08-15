e43eb7d24a17ec735c0419088c5d3a6b0865c704
ObaniGemini <robin@pi-et-ro.net>
2023-08-15 11:46:43 +0200

    Fix turret orientation

--- scenes/enemies/turrets/Turret.gd
+++ scenes/enemies/turrets/Turret.gd
@@ -8,16 +8,22 @@ const BULLET_SPEED = 300
 var EnemyBullet = preload("res://scenes/enemies/turrets/EnemyBullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
+@onready var BASE_ANGLE = rotation
+@onready var MIN_ANGLE = BASE_ANGLE - PI/3
+@onready var MAX_ANGLE = BASE_ANGLE + PI/3
+var ship = null
+
 func _ready():
+	if get_tree().current_scene.name == "World":
+		ship = get_tree().current_scene.level_scene.get_node("Ship")
+	else: ### That's for testing
+		ship = get_tree().current_scene.get_node("Ship")
 	$ShootingTimer.wait_time = randf_range(MIN_SHOOTING_TIME, MAX_SHOOTING_TIME)
 
 
 func _physics_process(_delta):
-	var ship = get_node("/root/World").level_scene.get_node("Ship")
-	var angle = $TurretBase.global_position.angle_to_point(ship.global_position)
-	if (rotation - angle) > PI or (rotation - angle) < -PI:
-		angle = rotation
-	rotation = angle
+	var angle = $TurretBase.global_position.angle_to_point(ship.global_position) + PI/2 - get_parent().global_rotation
+	rotation = clampf(angle, MIN_ANGLE, MAX_ANGLE)
 
 func damage(_hitpoint):
 	destroy()
@@ -33,6 +39,7 @@ func shooting_direction():
 	return ($TurretHead.global_position - $TurretBase.global_position).normalized()
 
 func shoot_bullet():
+	pass
 	var bullet: PhysicsBody2D = EnemyBullet.instantiate()
 	bullet.position = $TurretHead.global_position
 	bullet.linear_velocity = shooting_direction() * BULLET_SPEED
