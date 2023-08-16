7b55a807193e59477c851f50b4922ec5a393d671
Adrien Pouyet <Ricocotam@users.noreply.github.com>
2023-08-16 11:58:18 +0200

    Level 3 WIP

--- scenes/levels/Level3.gd
+++ scenes/levels/Level3.gd
@@ -2,3 +2,14 @@ extends "res://scenes/main/level.gd"
 
 func _ready():
 	$Ship.set_camera_mode($Ship.Camera_Follow)
+
+
+# Detect right room is finished to unlock right generator
+# Same for left room
+
+# Pipeline :
+# 1. R/L Room is destroyed
+# 2. Unlocks generators (remove shield)
+# 3. Generator 1 destroyed -> remove first reactor shield
+# 4. Generator 2 destroyed -> remove second reactor shield
+# 5. Reactor is exposed and can be destroyed
