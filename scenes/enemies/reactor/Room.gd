64b4a5994f2a553c371f752d33e97ac6fe3a5ebf
Adrien Pouyet <Ricocotam@users.noreply.github.com>
2023-08-17 09:03:05 +0200

    Level 3 wip wip

---
+++ scenes/enemies/reactor/Room.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+signal room_cleared
+
+var nb_console_destroyed = 0
+
+
+func destroy_room():
+	# Room destruction animation ?
+	room_cleared.emit()
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if nb_console_destroyed == 3:
+		destroy_room()
+		nb_console_destroyed = -1
+
+func _on_console_destroyed():
+	nb_console_destroyed += 1
